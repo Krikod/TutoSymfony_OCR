39e21c66d4f09ecf50073a78c7b5301daae6ce3f
kp <kristen.kod@gmail.com>
2017-07-19 11:19:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+TutoSymfony
+===========
+
+A Symfony project created on April 25, 2017, 2:12 pm.
---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,115 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface; // pas dans le cours...?
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AdvertController extends Controller
+{
+    public function addAction(Request $request)
+    {
+        // On crée un objet Advert
+        $advert = new Advert();
+
+        // On crée le formBuilder grâce au service form factory
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $advert);
+
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+        $formBuilder
+            ->add('date'),      DateType::class)
+            ->add('title'),     TextType::class)
+            ->add('content'),   TextareaType::class)
+            ->add('author'),    TextType::class)
+            ->add('published'), CheckboxType::class)
+            ->add('save'),      SubmitType::class)
+        ;
+        // Pour l'instant, pas de candidatures, catégories, etc., on les gèrera plus tard
+
+        // A partir du formBuilder, on génère le formulaire
+        $form = $formBuilder->getForm();
+
+        // On passe la méthode createView() du formulaire à la vue
+        // afin qu'elle puisse afficher le formulaire toute seule
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+    /*
+    public function addAction(Request $request)
+    {
+        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if ($request->isMethod('POST'))
+        {
+            // Ici, on s'occupera de la création et de la gestion du formulaire
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+            // Puis on redirige vers la page de visualisation de cettte annonce
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        // Si on n'est pas en POST, alors on affiche le formulaire
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+    */
+    public function indexAction($page)
+    {
+        // On ne sait pas combien de pages il y a
+        // Mais on sait qu'une page doit être supérieure ou égale à 1
+        if ($page < 1)
+        {
+            // On déclenche une exception NotFoundHttpException, cela va afficher
+            // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+            throw new NotFoundHttpException('Page "'. $page.'" inexistante.');
+        }
+        // Ici, on récupérera la liste des annonces, puis on la passera au template
+
+        // Mais pour l'instant, on ne fait qu'appeler le template
+        return $this->render('OCPlatformBundle:Advert:index.html.twig');
+    }
+
+    // La route fait appel à OCPlatformBundle:Advert:view ==> définir la méthode viewAction.
+    // On donne à cette méthode l'argument $id, pour correspondre au paramètre {id} de la route
+    public function viewAction($id) // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+    {
+        // Ici, on récupèrera depuis la base de données l'annonce correspondant à l'id $id.
+        // Puis on passera l'annonce à la vue pour qu'elle puisse l'afficher
+        //return new Response("Affichage de l'annonce d'id: " .$id);
+        // Ici, on récupérera l'annonce correspondante à l'id $id
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'id' => $id
+        ));
+    }
+
+    public function editAction ($id, Request $request)
+    {
+        // Ici, on récupérera l'annonce correspondante à $id
+
+        // Même mécanisme que pour l'ajout
+        if ($request->isMethod('POST'))
+        {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    }
+
+    public function deleteAction ($id)
+    {
+        // Ici, on récupérera l'annonce correspondant à $id
+
+        // Ici, on gérera la suppression de l'annonce en question
+        return $this->render('OCPlatformBundle;Advert:delete.html.twig');
+    }
+}
